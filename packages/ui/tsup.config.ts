4e6e46918ba6f9518ed62efe34598a8c87cc244f
Sam Maister <goosewobbler@protonmail.com>
2025-05-22 03:12:09 +0100

    chore: add core to external

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -4,7 +4,14 @@ export default defineConfig({
   entry: ['src/index.ts', 'src/electron.ts', 'src/tauri.ts'],
   format: ['esm', 'cjs'],
   dts: true,
-  external: ['react', 'react-dom', '@zubridge/electron', '@zubridge/tauri', '@tauri-apps/api/webviewWindow'],
+  external: [
+    'react',
+    'react-dom',
+    '@zubridge/electron',
+    '@zubridge/tauri',
+    '@zubridge/core',
+    '@tauri-apps/api/webviewWindow',
+  ],
   noExternal: ['clsx'],
   clean: true,
 });
