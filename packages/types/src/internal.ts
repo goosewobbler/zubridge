2245e5b878e0108182df6acd645274221f93f7b9
Sam Maister <goosewobbler@protonmail.com>
2025-06-12 00:04:33 +0100

    refactor(types): remove __zubridge_thunkProcessor

--- packages/types/src/internal.ts
+++ packages/types/src/internal.ts
@@ -6,20 +6,6 @@ import type { Action, AnyState, DispatchOptions, Handlers, InternalThunk } from 
  */
 export interface ZubridgeInternalWindow {
   /**
-   * Internal thunk processor exposed by preload
-   */
-  __zubridge_thunkProcessor?: {
-    executeThunk: <S extends AnyState>(
-      thunk: InternalThunk<S>,
-      getState: () => S | Promise<S>,
-      options?: DispatchOptions,
-      parentId?: string,
-    ) => Promise<any>;
-    completeAction: (actionId: string, result: any) => void;
-    dispatchAction: (action: string | Action, payload?: unknown, parentId?: string) => Promise<void>;
-  };
-
-  /**
    * Internal subscription validator exposed by preload
    */
   __zubridge_subscriptionValidator?: {
