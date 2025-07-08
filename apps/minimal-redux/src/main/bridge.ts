365bcb6db1e9f56b32941eb53292b094c28bbc95
Sam Maister <goosewobbler@protonmail.com>
2025-07-08 02:48:49 +0100

    chore: fix redux app

--- apps/minimal-redux/src/main/bridge.ts
+++ apps/minimal-redux/src/main/bridge.ts
@@ -1,5 +1,6 @@
 import { createReduxBridge, type ZustandBridge, type ZubridgeMiddleware } from '@zubridge/electron/main';
 import type { Store } from 'redux';
+import { actions } from './store.js';
 
 /**
  * Creates a bridge using a Redux store
@@ -8,9 +9,15 @@ import type { Store } from 'redux';
 export function createBridge(store: Store, middleware?: ZubridgeMiddleware): ZustandBridge {
   console.log('[Redux Mode] Creating bridge with Redux store');
 
-  // Create bridge with Redux store and the createReduxBridge function from the library
+  // Create bridge with Redux store and action mapping
   const bridge = createReduxBridge(store, {
     middleware,
+    handlers: {
+      // Map string actions to Redux action creators
+      'COUNTER:INCREMENT': () => store.dispatch(actions['COUNTER:INCREMENT']()),
+      'COUNTER:DECREMENT': () => store.dispatch(actions['COUNTER:DECREMENT']()),
+      'THEME:TOGGLE': () => store.dispatch(actions['THEME:TOGGLE']()),
+    },
   });
 
   return bridge;
