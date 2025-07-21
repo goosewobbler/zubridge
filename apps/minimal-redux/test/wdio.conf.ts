fd20dd87fd27cb038f93ae6039b093f329debae0
Sam Maister <goosewobbler@protonmail.com>
2025-07-21 13:23:34 +0100

    chore: simplify wdio configs

--- apps/minimal-redux/test/wdio.conf.ts
+++ apps/minimal-redux/test/wdio.conf.ts
@@ -1,17 +1,9 @@
 import url from 'node:url';
 import path from 'node:path';
-import fs from 'node:fs';
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
-// App directory is parent directory (since config is now in test/ subdirectory)
-const appDir = path.dirname(__dirname);
-const appName = 'minimal-redux';
-
-// Test specs location
 const testSpecs = [path.join(__dirname, 'specs', '**/*.spec.ts')];
-
-// Check app entry point - using relative path
 const appEntryPoint = './out/main/index.js';
 
 const config: any = {
@@ -24,21 +16,6 @@ const config: any = {
       'browserName': 'electron',
       'wdio:electronServiceOptions': {
         appEntryPoint: appEntryPoint,
-      },
-      'goog:loggingPrefs': {
-        browser: 'ALL',
-        driver: 'ALL',
-      },
-      'goog:chromeOptions': {
-        args: [
-          '--no-sandbox',
-          '--disable-dev-shm-usage',
-          '--disable-gpu',
-          '--disable-web-security',
-          '--disable-features=VizDisplayCompositor',
-          '--headless=new',
-          '--user-data-dir=/tmp/wdio-electron-' + Date.now(),
-        ],
       },
     },
   ],
