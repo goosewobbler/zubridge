937a7ad75a41d435aa97c70286f4c1830aed5ac7
Sam Maister <goosewobbler@protonmail.com>
2025-07-30 11:48:38 +0100

    chore: use tsup for esm / cjs builds

---
+++ packages/types/tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: {
+    index: 'src/index.ts',
+    internal: 'src/internal.ts',
+    app: 'src/app.ts',
+  },
+  format: ['esm', 'cjs'],
+  dts: true,
+  clean: true,
+  splitting: false,
+  sourcemap: false,
+  minify: false,
+  external: ['zustand', 'electron'],
+});
