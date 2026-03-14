b91a1b9530944ddf9670e5121cddcfcfd6162dfb
Sam Maister <goosewobbler@protonmail.com>
2026-03-14 10:32:27 +0000

    refactor(electron): reorder imports and remove version from delta in SubscriptionHandler
    - Moved the import of randomUUID to the correct position for better organization.
    - Removed the version property from the delta object in SubscriptionHandler, streamlining the delta structure for improved clarity.

--- packages/electron/src/bridge/subscription/SubscriptionHandler.ts
+++ packages/electron/src/bridge/subscription/SubscriptionHandler.ts
@@ -193,7 +193,6 @@ export class SubscriptionHandler<State extends AnyState> {
         updateId,
         delta: {
           type: 'delta',
-          version: 1,
           changed: deltaChanged,
         },
         thunkId: undefined,
